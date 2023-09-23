fdf6bf39acc3e937b37766c7e87dac3c55114fab
Aryanmehta21 <aryanmehta310@gmail.com>
2023-09-23 20:24:00 +0530

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -13,10 +13,7 @@ const Footer = () => {
               <strong>BookStore</strong>
             </h6>
             <p>
-              Book-IT is an online React web application where the customer can
-              purchase books online. Through this book store the users can
-              search for a book by its title and later can add to the shopping
-              cart and finally purchase using credit card transaction.
+            BookStore is a dynamic online React-based platform designed for book enthusiasts. This web application empowers users to explore an extensive collection of books, conveniently search for titles they desire, and seamlessly add their selected reads to a virtual shopping cart. Ultimately, users can complete their literary journey by making secure credit card transactions to purchase their chosen books.
             </p>
           </MDBCol>
           <hr className="w-100 clearfix d-md-none" />
